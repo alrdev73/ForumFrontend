edc479f962545d1285e4eb424fec145f0d3bded4
alrdev73 <alintigaeru@gmail.com>
2024-05-17 15:46:22 +0100

    initial files

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+function App() {
+    return (
+    <>
+        <div className={"header"}>
+            <h1>Header</h1>
+        </div>
+        <div className={"body"}>
+            <h1>Body</h1>
+        </div>
+        <div className={"footer"}>
+            <h1>Footer</h1>
+        </div>
+    </>
+    )
+}
+
+export default App
